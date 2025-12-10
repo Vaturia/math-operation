e1e7e7f903107f9c43c7eb9fffb507879e98cf57
Vaturia <kkorzun19@gmail.com>
2025-12-10 18:47:53 +0300

    upd

--- include/math_library.h
+++ include/math_library.h
@@ -3,7 +3,7 @@ namespace math_operations {
 int addition(int a, int b, long long &result);
 int subtraction(int a, int b, long long &result);
 int multiplication(int a, int b, long long &result);
-int division(int a, int b, double &result);
+int division(int a, int b, long long &result);
 int exponentiation(int a, int b, long long &result);
 long long factorial(unsigned int a);
 };
