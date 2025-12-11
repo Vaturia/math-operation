753712789321833575a6ca10de9832b636083aea
Vaturia <kkorzun19@gmail.com>
2025-12-11 15:13:38 +0300

    clang-format

--- include/math_library.h
+++ include/math_library.h
@@ -1,9 +1,10 @@
 #pragma once
-namespace math_operations {
-int addition(int a, int b, long long &result);
-int subtraction(int a, int b, long long &result);
-int multiplication(int a, int b, long long &result);
-int division(int a, int b, long long &result);
-int exponentiation(int a, int b, long long &result);
-long long factorial(unsigned int a);
-}; // namespace math_operations
+namespace math_operations
+{
+    int addition(int a, int b, long long &result);
+    int subtraction(int a, int b, long long &result);
+    int multiplication(int a, int b, long long &result);
+    int division(int a, int b, long long &result);
+    int exponentiation(int a, int b, long long &result);
+    long long factorial(unsigned int a);
+};  // namespace math_operations
--- src/math_library.cpp
+++ src/math_library.cpp
@@ -1,37 +1,48 @@
 #include "math_library.h"
+
 #include <cstdlib>
-int math_operations::addition(int a, int b, long long &result) {
-  result = a + b;
-  return 0;
+int math_operations::addition(int a, int b, long long &result)
+{
+    result = a + b;
+    return 0;
 }
-int math_operations::subtraction(int a, int b, long long &result) {
-  result = a - b;
-  return 0;
+int math_operations::subtraction(int a, int b, long long &result)
+{
+    result = a - b;
+    return 0;
 }
-int math_operations::multiplication(int a, int b, long long &result) {
-  result = a * b;
-  return 0;
+int math_operations::multiplication(int a, int b, long long &result)
+{
+    result = a * b;
+    return 0;
 }
-int math_operations::division(int a, int b, long long &result) {
-  if (b == 0) {
-    return -1;
-  }
-  result = a / b;
-  return 0;
+int math_operations::division(int a, int b, long long &result)
+{
+    if (b == 0)
+    {
+        return -1;
+    }
+    result = a / b;
+    return 0;
 }
-int math_operations::exponentiation(int a, int b, long long &result) {
-  if (b < 0) {
-    return -1;
-  };
-  result = 1;
-  for (size_t i = 0; i < b; ++i) {
-    result *= a;
-  }
-  return 0;
+int math_operations::exponentiation(int a, int b, long long &result)
+{
+    if (b < 0)
+    {
+        return -1;
+    };
+    result = 1;
+    for (size_t i = 0; i < b; ++i)
+    {
+        result *= a;
+    }
+    return 0;
 }
-long long math_operations::factorial(unsigned int a) {
-  if (a > 1) {
-    return a * (factorial(a - 1));
-  }
-  return 1;
+long long math_operations::factorial(unsigned int a)
+{
+    if (a > 1)
+    {
+        return a * (factorial(a - 1));
+    }
+    return 1;
 }
